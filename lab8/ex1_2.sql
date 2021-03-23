fbadaf0520bdcefa6bd9b6ab0941599c2cba27ae
asleepann <66643655+asleepann@users.noreply.github.com>
2021-03-23 13:56:33 +0300

    Update ex1_2.sql

--- lab8/ex1_2.sql
+++ lab8/ex1_2.sql
@@ -1,23 +1,24 @@
+-- Add field bank_name
 ALTER TABLE account ADD COLUMN bank_name VARCHAR;
 UPDATE account SET bank_name = 'SpearBank'
-WHERE account_id = 0 OR account_id = 2;
+WHERE account_id = 1 OR account_id = 3;
 UPDATE account SET bank_name = 'Tinkoff'
-WHERE account_id = 1;
+WHERE account_id = 2;
 
-INSERT INTO account (account_id, customer_name, credit)
+INSERT INTO account (customer_name, credit)
 VALUES
-(3, 'Fees', 0);
+('Fees', 0);
 
--- Transactioin 1
+-- Transaction 1
 START TRANSACTION;
 
 UPDATE account
 	SET credit = credit - 500
-	WHERE account_id = 0;
+	WHERE account_id = 1;
 	
 UPDATE account
 	SET credit = credit + 500
-	WHERE account_id = 2;
+	WHERE account_id = 3;
 	
 SELECT * FROM account;
 
@@ -28,15 +29,15 @@ START TRANSACTION;
 
 UPDATE account
 	SET credit = credit - (700 + 30)
-	WHERE account_id = 1;
+	WHERE account_id = 2;
 	
 UPDATE account
 	SET credit = credit + 30
-	WHERE account_id = 3;
+	WHERE account_id = 4;
 
 UPDATE account
 	SET credit = credit + 700
-	WHERE account_id = 0;
+	WHERE account_id = 1;
 	
 SELECT * FROM account;
 
@@ -46,16 +47,16 @@ ROLLBACK;
 START TRANSACTION;
 
 UPDATE account
-	SET credit = credit - (1000 + 30)
-	WHERE account_id = 1;
+	SET credit = credit - (100 + 30)
+	WHERE account_id = 2;
 	
 UPDATE account
 	SET credit = credit + 30
-	WHERE account_id = 3;
+	WHERE account_id = 4;
 	
 UPDATE account
-	SET credit = credit + 1000
-	WHERE account_id = 2;
+	SET credit = credit + 100
+	WHERE account_id = 3;
 	
 SELECT * FROM account;
 
