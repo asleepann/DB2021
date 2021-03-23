59260a1e457b6e13f6723d1afaac153b4108dde2
asleepann <66643655+asleepann@users.noreply.github.com>
2021-03-23 11:57:36 +0300

    Update ex1_2.sql

--- lab8/ex1_2.sql
+++ lab8/ex1_2.sql
@@ -1,1 +1,6 @@
+ALTER TABLE account ADD COLUMN bank_name VARCHAR;
+UPDATE account SET bank_name = 'SpearBank'
+WHERE account_id = 0 OR account_id = 2;
+UPDATE account SET bank_name = 'Tinkoff'
+WHERE account_id = 1;
 
