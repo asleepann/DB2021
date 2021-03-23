a7817c959bf77aaba05cec43883f7221c156cf48
asleepann <66643655+asleepann@users.noreply.github.com>
2021-03-23 12:15:29 +0300

    Update ex1_2.sql

--- lab8/ex1_2.sql
+++ lab8/ex1_2.sql
@@ -4,3 +4,59 @@ WHERE account_id = 0 OR account_id = 2;
 UPDATE account SET bank_name = 'Tinkoff'
 WHERE account_id = 1;
 
+INSERT INTO account (account_id, customer_name, credit)
+VALUES
+(3, 'Fees', 0);
+
+-- Transactioin 1
+START TRANSACTION;
+
+UPDATE account
+	SET credit = credit - 500
+	WHERE account_id = 0;
+	
+UPDATE account
+	SET credit = credit + 500
+	WHERE account_id = 2;
+	
+SELECT * FROM account;
+
+ROLLBACK;
+
+-- Transaction 2
+START TRANSACTION;
+
+UPDATE account
+	SET credit = credit - (700 + 30)
+	WHERE account_id = 1;
+	
+UPDATE account
+	SET credit = credit + 30
+	WHERE account_id = 3;
+
+UPDATE account
+	SET credit = credit + 700
+	WHERE account_id = 0;
+	
+SELECT * FROM account;
+
+ROLLBACK;
+
+-- Transaction 3
+START TRANSACTION;
+
+UPDATE account
+	SET credit = credit - (1000 + 30)
+	WHERE account_id = 1;
+	
+UPDATE account
+	SET credit = credit + 30
+	WHERE account_id = 3;
+	
+UPDATE account
+	SET credit = credit + 1000
+	WHERE account_id = 2;
+	
+SELECT * FROM account;
+
+ROLLBACK;
