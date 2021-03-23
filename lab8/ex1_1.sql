1f82479b11e2c16f6c68f882b18f79f44b868ba5
asleepann <66643655+asleepann@users.noreply.github.com>
2021-03-23 11:38:14 +0300

    Update ex1_1.sql

--- lab8/ex1_1.sql
+++ lab8/ex1_1.sql
@@ -1,1 +1,50 @@
+INSERT INTO account (account_id, customer_name, credit)
+VALUES
+(0, 'Anna', 1000),
+(1, 'Andrew', 1000),
+(2, 'Olga', 1000);
 
+-- Transactioin 1
+START TRANSACTION;
+
+UPDATE account
+	SET credit = credit - 500
+	WHERE account_id = 0;
+	
+UPDATE account
+	SET credit = credit + 500
+	WHERE account_id = 2;
+	
+SELECT * FROM account;
+
+ROLLBACK;
+
+-- Transaction 2
+START TRANSACTION;
+
+UPDATE account
+	SET credit = credit - 700
+	WHERE account_id = 1;
+	
+UPDATE account
+	SET credit = credit + 700
+	WHERE account_id = 0;
+	
+SELECT * FROM account;
+
+ROLLBACK;
+
+-- Transaction 3
+START TRANSACTION;
+
+UPDATE account
+	SET credit = credit - 1000
+	WHERE account_id = 1;
+	
+UPDATE account
+	SET credit = credit + 1000
+	WHERE account_id = 2;
+	
+SELECT * FROM account;
+
+ROLLBACK;
